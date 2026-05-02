8c4709528b8ca706aae1bce0c48cff1d7fde2dec
Wesley Castro <54373294+wesleycstr@users.noreply.github.com>
2026-05-02 17:51:44 -0300

    Create buscar_config.php

---
+++ apache/buscar_config.php
@@ -0,0 +1,25 @@
+<?php
+
+include 'conexao.php';
+
+$id = $_GET['id'];
+
+$sql = "
+
+SELECT
+    temperatura_max,
+    umidade_min,
+    umidade_max,
+    gas_max
+FROM sensores
+WHERE id = '$id'
+
+";
+
+$result = $conn->query($sql);
+
+$row = $result->fetch_assoc();
+
+echo json_encode($row);
+
+?>
